4225cd0f49985abe335f647b81d4a204d2fb2d94
Will <w255li@edu.uwaterloo.ca>
2019-07-28 15:47:25 -0400

    Update Chapter3.R

--- Chapter3.R
+++ Chapter3.R
@@ -5,7 +5,6 @@
 data<-read.table('/Users/will/Documents/GitHub/STAT-332-Experimental-Design/Data/fac_example1.txt', header = TRUE, sep = "", dec = ".")
 summary(aov(data$time~as.factor(data$tt)))
 aggregate(data$time, list(treatments = data$tt), mean)
-
 interaction.plot(data$method,data$sol,data$time,
                  type = 'b', pch=c(19,1),col = 1:2, lty =1:2)
 
