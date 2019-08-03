9690c400058fffe4b0a0e3be7c3ccefe44b5dee2
Will <w255li@edu.uwaterloo.ca>
2019-08-03 15:21:05 -0400

    Question 4b done

--- Chapter6.R
+++ Chapter6.R
@@ -1,4 +1,5 @@
 #question 4
+#a
 data<-read.table('/Users/will/Documents/GitHub/STAT-332-Experimental-Design/Data/thrush.txt', header = TRUE, sep = "", dec = ".")
 plot(data)
 attach(data)
@@ -12,13 +13,15 @@ n=50
 ratio_estimate = mu_hat_y/mu_hat_x*mu_x
 ratio_estimate*1783
 theta=mu_hat_y/mu_hat_x
-theta
 ri= (number-(area*theta))^2
+sigma=sqrt(sum(ri)/(n-1))
+standard_error=sigma*(sqrt((1-f)/n))
+confidence_interval = (ratio_estimate+qnorm(0.975)*standard_error)*1783
+#b
+head(data)
+beta = coef(lm(number~area))[2]
+ri=(number-(area-mu_hat_x)*beta - mu_hat_y)^2
 ri
 sigma=sqrt(sum(ri)/(n-1))
-sigma
 standard_error=sigma*(sqrt((1-f)/n))
-standard_error
-
 confidence_interval = (ratio_estimate+qnorm(0.975)*standard_error)*1783
-qnorm(0.975)
